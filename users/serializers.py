3545a6f5c0b14e02aec4151f36712bd3da0acd5b
Simon Chanzu <chanzusimon6@gmail.com>
2025-05-05 19:52:33 +0300

    commits

--- users/serializers.py
+++ users/serializers.py
@@ -4,5 +4,5 @@ from .models import CustomUser
 class CustomUserSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomUser
-        fields = ['id', 'username', 'email', 'phone_number', 'is_admin']
+        fields = ['id', 'username', 'email', 'phone_number', 'is_admin','is_delivery_person']
         read_only_fields = ['id', 'is_admin']
