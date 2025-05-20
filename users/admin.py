ca1f6335c8f25c709d99cd1c069efd37f4338cf6
Simon Chanzu <chanzusimon6@gmail.com>
2025-05-20 13:02:16 +0300

    commits

--- users/admin.py
+++ users/admin.py
@@ -4,7 +4,14 @@ from .models import CustomUser
 
 @admin.register(CustomUser)
 class CustomUserAdmin(admin.ModelAdmin):
-    list_display = ('username', 'email', 'phone_number', 'role', 'date_joined')
+    list_display = ('id', 'username', 'email', 'phone_number', 'role', 'is_active', 'date_joined')
     search_fields = ('username', 'email', 'phone_number')
-    list_filter = ('role', 'date_joined')
+    list_filter = ('role', 'is_active', 'date_joined')
+    list_editable = ('role', 'is_active')
+    readonly_fields = ('date_joined',)
     fields = ('username', 'email', 'phone_number', 'role', 'is_active', 'is_staff', 'groups', 'user_permissions')
+    ordering = ('username',)
+
+    def get_search_results(self, request, queryset, search_term):
+        queryset, use_distinct = super().get_search_results(request, queryset, search_term)
+        return queryset, False
