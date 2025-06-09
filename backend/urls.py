25a992326ebd51b43bc30693f24f4c9267627af4
Simon Chanzu <chanzusimon6@gmail.com>
2025-06-09 11:56:54 +0300

    commits

--- backend/urls.py
+++ backend/urls.py
@@ -2,10 +2,10 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-
-
+from users.views import GoogleLoginView
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('auth/google/callback/', GoogleLoginView.as_view(), name='google_callback'),
     path('api/users/', include('users.urls')), 
     path('api/', include('products.urls')), 
     path('api/', include('orders.urls')), 
--- users/urls.py
+++ users/urls.py
@@ -3,7 +3,6 @@ from django.urls import path
 from .views import (
     RegisterView,
     LoginView,
-    GoogleLoginView,
     UserProfileView,
     UserProfileUpdateView,
     AdminUserListCreateView,
@@ -14,7 +13,6 @@ from .views import (
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', LoginView.as_view(), name='login'),
-    path('google/', GoogleLoginView.as_view(), name='google_login'),
     path('profile/', UserProfileView.as_view(), name='profile'),
     path('profile/update/', UserProfileUpdateView.as_view(), name='profile-update'),
     path('manage/users/', AdminUserListCreateView.as_view(), name='manage-user-list'),
