dabe759d7c192b366b2e33a31ab8f02f3cb935b1
Simon Chanzu <chanzusimon6@gmail.com>
2025-06-12 11:20:08 +0300

    commits

--- users/views.py
+++ users/views.py
@@ -166,7 +166,7 @@ class GoogleLoginView(APIView):
             serializer = CustomUserSerializer(user)
 
             # Redirect to frontend with tokens
-            frontend_url = 'https://muindi-mweusi.onrender.com/home/'
+            frontend_url = 'https://muindi-mweusi.onrender.com/'
             params = {
                 'access': str(refresh.access_token),
                 'refresh': str(refresh),
